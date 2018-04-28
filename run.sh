0f94968ac1a32b4fafcd939387d0ac0f8c6fc582
Anton Markelov <doublic@gmail.com>
2018-04-28 19:53:47 +1000

    fix tmp dir

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 cp /usr/share/zoneinfo/$TZ /etc/localtime 
 echo $TZ > /etc/timezone
 
+mkdir /tmp/.X11-unix
+chmod 1777 /tmp/.X11-unix
+chown root /tmp/.X11-unix/
+
 Xvfb :99 -screen 0 1280x1024x8 -nolisten tcp &
 /tests/gradlew runIntegrationTestBrowser
